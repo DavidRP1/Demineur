cc847fd2ced1b742ba379623fea3b777729b64e5
Zoe Wang <zoewang@zoes-macbook-pro.local>
2018-10-23 12:48:13 -0400

    Implementer la fonction afficherImage.

--- demineur.js
+++ demineur.js
@@ -3,10 +3,18 @@
 // Description du programme
 
 ////////////////////////////////////////////////////////////////////////////////
+load("image.js");
 
-
-var  afficherImage = function(x, y, colormap, image){
-}:
+var afficherImage = function(x, y, colormap, image){
+    for(var i = 0; i <image.length; i++ ){
+        var rang = image[i];
+        for(var j = 0; j < rang.length; j++){
+            var color = colormap[rang[j]];
+            setPixel(j, i, color);
+        }
+    }
+    
+};
 
 
 var attendreClic = function(){
@@ -18,12 +26,21 @@ var placerMines = function(largeur, hauteur, nbMines, x, y){
 
 
 var demineur = function(largeur, hauteur, nbMines){
+    
 };
 
 
 // tests unitaires de la procedure afficherImage et la fonction placerMines
 var testDemineur = function(){
+    setScreenMode(16, 16);
+    afficherImage(1,1,colormap,images[7]);
 };
 
+
 testDemineur();
+
+
+
+
+
 
