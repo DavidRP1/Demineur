1c076b93ab7448148ecd56f562eb224486f81476
DavidRP1 <36389670+DavidRP1@users.noreply.github.com>
2018-10-22 20:22:57 -0400

    Add files via upload

---
+++ demineur.js
@@ -0,0 +1,29 @@
+// Raby-Pepin, David & Wang, Xiaoqian
+// 05-11-2018
+// Description du programme
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+var  afficherImage = function(x, y, colormap, image){
+}:
+
+
+var attendreClic = function(){
+};
+
+
+var placerMines = function(largeur, hauteur, nbMines, x, y){
+};
+
+
+var demineur = function(largeur, hauteur, nbMines){
+};
+
+
+// tests unitaires de la proc´edure afficherImage et la fonction placerMines
+var testDemineur = function(){
+};
+
+testDemineur();
+
