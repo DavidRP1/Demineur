ba53f99cce8afae44c0ec3a400aae1d74fc3e552
ZoeWang0505 <40203903+ZoeWang0505@users.noreply.github.com>
2018-10-24 01:47:57 -0400

    Developer function demineur pour Afficher images.

--- demineur.js
+++ demineur.js
@@ -6,21 +6,83 @@
 
 load("image.js");
 
+
 var demineur = function(largeur, hauteur, nbMines){
     setScreenMode(largeur*16, hauteur*16);
     
     var positionClic = attendreClic();
     
-    placerMines(largeur, hauteur, nbMines, positionClic.x, positionClic.y);
+    var tableauMines = placerMines(largeur, hauteur, nbMines, positionClic.x, positionClic.y);
+    
+    var tableauAfficher = [];
+    
+    //Pour construire le tableau d'afficher qui donner le numero d'image
+    //tel que 0, 1,2,3,4,5,6,7,8,mine
+    for(var i = 0; i < tableauMines.length; i++){
+        var rang = tableauMines[i];
+        var rangImage = [];
+        for(var j = 0; j < rang.length; j++){
+            // si c'est une mine, utilise image 9
+            if (tableauMines[i][j] == true){
+                print("i, j", i, j);
+                rangImage.push(9);
+                continue;
+            }
+            //ce n'est pas une mine, donc
+            //compter la numbre de mines autour de cette position
+            var mines = 0;
+            if ( i - 1 >= 0){
+                mines = tableauMines[i-1][j] == true ? mines + 1 : mines;
+                if(j - 1 >= 0){
+                    mines = tableauMines[i - 1][j - 1] == true ? mines + 1 : mines;
+                    mines = tableauMines[i][j - 1] == true ? mines + 1 : mines;
+                }
+                if(j + 1 < rang.length){
+                    mines = tableauMines[i - 1][j + 1] == true ? mines + 1 : mines;
+                    mines = tableauMines[i][j + 1] == true ? mines + 1 : mines;
+                }
+            }
+            if ( i + 1 < tableauMines.length){
+                mines = tableauMines[i + 1][j] == true ? mines + 1 : mines;
+                if(j + 1 < rang.length){
+                    mines = tableauMines[i + 1][j + 1] == true ? mines + 1 : mines;
+                }
+                if(j - 1 >= 0){
+                    mines = tableauMines[i + 1][j - 1] == true ? mines + 1 : mines;
+                }
+            }
+            rangImage.push(mines);
+        }
+        tableauAfficher.push(rangImage);
+    }
+    
+    //Dessiner intielement
+    for(var i = 0; i < largeur; i++){
+        for(var j = 0; j < hauteur; j++){
+            afficherImage(i * 16, j * 16, colormap, images[11]);
+        }
+    }
     
     // a complete
-    while (/reste des tuiles/){
-        afficherImage(x, y, colormap, image)
+    var fini = false;
+    while (!fini){
         positionClic = attendreClic();
+        if(tableauMines[positionClic.x][positionClic.y] == false){
+            //Redessiner les champs
+            
+        } else {
+            fini = true;
+            //Redesiner le map
+            for(var i = 0; i < tableauAfficher.length; i++){
+                var rang = tableauAfficher[i];
+                for(var j = 0; j < rang.length; j++){
+                    afficherImage(j * 16, i * 16, colormap, images[tableauAfficher[i][j]]);
+                }
+            }
+        }
     }
     
 };
-
 
 var attendreClic = function(){
     var clic = getMouse().down;
@@ -100,7 +162,7 @@ var afficherImage = function(x, y, colormap, image){
         var rang = image[i];
         for(var j = 0; j < rang.length; j++){
             var color = colormap[rang[j]];
-            setPixel(j, i, color);
+            setPixel(x+j, y+i, color);
         }
     }
 };
@@ -110,10 +172,12 @@ var afficherImage = function(x, y, colormap, image){
 
 // tests unitaires de la procedure afficherImage et la fonction placerMines
 var testDemineur = function(){
-    setScreenMode(16, 16);
-    afficherImage(1,1,colormap,images[7]);
+    demineur(5,3,3);
+    //afficherImage(1,1,colormap,images[7]);
 };
 
 
 testDemineur();
+
+
 
