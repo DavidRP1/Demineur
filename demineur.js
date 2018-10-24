a1d792c9826e89a2b68869ecdb2780a374828c21
ZoeWang0505 <40203903+ZoeWang0505@users.noreply.github.com>
2018-10-23 21:21:43 -0400

    Revert "Implementer la fonction afficherImage."
    This reverts commit cc847fd2ced1b742ba379623fea3b777729b64e5.

--- demineur.js
+++ demineur.js
@@ -3,18 +3,10 @@
 // Description du programme
 
 ////////////////////////////////////////////////////////////////////////////////
-load("image.js");
 
-var afficherImage = function(x, y, colormap, image){
-    for(var i = 0; i <image.length; i++ ){
-        var rang = image[i];
-        for(var j = 0; j < rang.length; j++){
-            var color = colormap[rang[j]];
-            setPixel(j, i, color);
-        }
-    }
-    
-};
+
+var  afficherImage = function(x, y, colormap, image){
+}:
 
 
 var attendreClic = function(){
@@ -26,21 +18,12 @@ var placerMines = function(largeur, hauteur, nbMines, x, y){
 
 
 var demineur = function(largeur, hauteur, nbMines){
-    
 };
 
 
 // tests unitaires de la procedure afficherImage et la fonction placerMines
 var testDemineur = function(){
-    setScreenMode(16, 16);
-    afficherImage(1,1,colormap,images[7]);
 };
 
-
 testDemineur();
-
-
-
-
-
 
